3fdc47a651acbb8e04fca0d6b538bce594bb6c21
Woo <leo9634@naver.com>
2021-06-04 07:02:46 -0400

    config client ready -> listen to config server port 8888 -> config server fetches application.properties from github repo

--- src/main/java/com/microservice/search/SearchApplication.java
+++ src/main/java/com/microservice/search/SearchApplication.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SearchApplication implements CommandLineRunner {
 	private static final Logger logger = LoggerFactory.getLogger(SearchApplication.class);
 
